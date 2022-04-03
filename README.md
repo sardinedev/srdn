e389160556f42b1bd492908560026588681f2725
Hugo Nogueira <hnogueira@marabyte.com>
2022-04-03 18:50:26 +0000

    feat: builds files from directory

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ The Sardine CLI
 ```bash
 cargo run -- --option [command]
 ```
+
+### Builds all files in `tests/src` and outputs them in `tests/dist`
+```bash
+cargo run -- build -s tests/src -d tests/dist
+```
+
+### Builds one file and outputs it in `tests/dist/a/a.css`
+```bash
+cargo run -- build -f ./tests/src/a/a.module.css -o tests/dist/a/a.css
+```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use browserslist::{resolve, Opts};
 use clap::{Parser, Subcommand};
+use glob::glob;
 use parcel_css::stylesheet::{MinifyOptions, ParserOptions, PrinterOptions, StyleSheet};
 use parcel_css::targets::Browsers;
-use serde_json::json;
-use std::{fs, path::PathBuf};
+use serde_json::{json, Value};
+use std::error::Error;
+use std::{fmt, fs, path::PathBuf};
 
 /// 4 in 5 sardines recommend this CLI
 #[derive(Parser)]
@@ -26,16 +28,16 @@ enum Commands {
     /// Builds your project
     Build {
         /// The relative path to a file
-        #[clap(short, long, value_name = "FILE", group = "output_file")]
+        #[clap(short, long, value_name = "FILE")]
         file: Option<String>,
         /// Destination file for the output
-        #[clap(short, long, requires = "output_file")]
+        #[clap(short, long)]
         output_file: Option<PathBuf>,
         /// The relative path to the source directory
         #[clap(short = 's', long, value_name = "DIR")]
         dir: Option<String>,
         /// Destination directory for the output
-        #[clap(short = 'd', long, group = "output_directory")]
+        #[clap(short = 'd', long)]
         output_dir: Option<PathBuf>,
     },
 }
@@ -58,53 +60,37 @@ fn main() -> Result<(), std::io::Error> {
         Some(Commands::Build {
             file,
             output_file,
-            dir: _,
-            output_dir: _,
+            dir,
+            output_dir,
         }) => {
-            if let Some(file_path) = file.as_deref() {
-                let source = fs::read_to_string(file_path);
-                let contents = source.unwrap();
-
-                let options = ParserOptions {
-                    nesting: true,
-                    css_modules: file_path.contains(".module.css"),
-                    ..ParserOptions::default()
-                };
-                let mut stylesheet =
-                    StyleSheet::parse(file_path.into(), &contents, options).unwrap();
-
-                let targets = browserslist_to_targets(vec!["chrome 98".to_string()]).unwrap();
-                stylesheet
-                    .minify(MinifyOptions {
-                        targets,
-                        ..MinifyOptions::default()
-                    })
-                    .unwrap();
-
-                let res = stylesheet
-                    .to_css(PrinterOptions {
-                        targets,
-                        minify: true,
-                        ..PrinterOptions::default()
-                    })
-                    .unwrap();
-                if let Some(dist_path) = output_file {
-                    let code = res.code;
-
-                    // creates all sub-directories if they don't exist yet
-                    if let Some(parent_dir) = dist_path.parent() {
-                        fs::create_dir_all(parent_dir)?
-                    };
-
-                    // writes file to disk
-                    fs::write(dist_path, code.as_bytes())?;
-
-                    // generates a map for the CSS Modules
-                    let modules_map = json!(res.exports);
-                    println!("{}", modules_map);
-                } else {
-                    println!("{}", res.code);
+            // reads and build an individual css file
+            if let Some(file_path) = file {
+                if let Some(file_path_out) = output_file {
+                    match build_css(&PathBuf::from(file_path), file_path_out) {
+                        Ok(css_modules_map) => println!("{}", css_modules_map),
+                        Err(e) => println!("{:?}", e),
+                    }
                 }
+            }
+
+            // reads and builds all css files in a directory
+            if let Some(src_dir) = dir.as_deref() {
+                let dir = &fmt::format(format_args!("{}/**/*.css", src_dir));
+                if let Some(dist) = output_dir {
+                    for entry in glob(dir).expect("Failed to read glob pattern") {
+                        match entry {
+                            Ok(file_path) => {
+                                println!("{:?}", file_path);
+                                match build_css(&file_path, dist) {
+                                    Ok(css_modules_map) => println!("{}", css_modules_map),
+                                    Err(e) => println!("{:?}", e),
+                                }
+                            }
+                            Err(e) => println!("{:?}", e),
+                        }
+                    }
+                 }
+
             }
         }
 
@@ -112,6 +98,58 @@ fn main() -> Result<(), std::io::Error> {
     }
     Ok(())
 }
+
+fn build_css(path_to_file: &PathBuf, path_to_output: &PathBuf) -> Result<Value, Box<dyn Error>> {
+    let src_file_path = path_to_file.to_str().unwrap();
+    let mut output_file_path = PathBuf::new();
+    output_file_path.push(path_to_output);
+
+    if !path_to_output.is_file() {
+        output_file_path.push(path_to_file);
+    }
+    println!("Dist {:?}", output_file_path.display());
+    println!("Source {:?}", path_to_file.display());
+
+    let source = fs::read_to_string(path_to_file);
+    let contents = source.unwrap();
+
+    let options = ParserOptions {
+        nesting: true,
+        css_modules: src_file_path.contains(".module.css"),
+        ..ParserOptions::default()
+    };
+    let mut stylesheet = StyleSheet::parse(src_file_path.into(), &contents, options).unwrap();
+
+    let targets = browserslist_to_targets(vec!["chrome 98".to_string()]).unwrap();
+    stylesheet
+        .minify(MinifyOptions {
+            targets,
+            ..MinifyOptions::default()
+        })
+        .unwrap();
+
+    let res = stylesheet
+        .to_css(PrinterOptions {
+            targets,
+            minify: true,
+            ..PrinterOptions::default()
+        })
+        .unwrap();
+    let code = res.code;
+
+    // creates all sub-directories if they don't exist yet
+    if let Some(parent_dir) = output_file_path.parent() {
+        fs::create_dir_all(parent_dir)?;
+    };
+
+    // writes file to disk
+    fs::write(output_file_path, code.as_bytes())?;
+
+    // generates a map for the CSS Modules
+    let modules_map = json!(res.exports);
+    Ok(modules_map)
+}
+
 // This should from package.json
 fn browserslist_to_targets(query: Vec<String>) -> Result<Option<Browsers>, browserslist::Error> {
     if query.is_empty() {
